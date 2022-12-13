78057937c4acc5174ca3f293da3f0dc1e197f511
Quentin Laffont <qlaffont13@gmail.com>
2022-12-13 09:10:42 +0100

    chore(app): release 1.0.0

--- WindowsLayoutSnapshot/Properties/AssemblyInfo.cs
+++ WindowsLayoutSnapshot/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsLayoutSnapshot")]
+[assembly: AssemblyTitle("WindowsLayoutSnapshot by LeStudio")]
 [assembly: AssemblyDescription("Windows app to remember + restore window positions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsLayoutSnapshot")]
-[assembly: AssemblyCopyright("public domain ⓪ 2012-2018")]
+[assembly: AssemblyProduct("WindowsLayoutSnapshot by LeStudio")]
+[assembly: AssemblyCopyright("public domain ⓪ 2012-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
